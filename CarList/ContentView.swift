9fc2d5f8d19e9b44bc29020898dc3d2e0f993a03
SeongKookKIM <ksk8646@naver.com>
2024-04-16 17:25:00 +0900

    Protocol && Extention사용

--- CarList/ContentView.swift
+++ CarList/ContentView.swift
@@ -1,23 +1,14 @@
-//
-//  ContentView.swift
-//  CarList
-//
-//  Created by SeongKook on 4/16/24.
-//
-
 import SwiftUI
 
-protocol Fuel {
-    var fuelEfficiency: Double { get set } // 연비
-    var isGasoline: Bool { get set } // 가솔린일까 디젤일까
-    var fuelEfficiencyString: String { get }
-    var isGasolineString: String { get }
-}
-protocol Electric {
-    var autoLevel: Int { set get }
+protocol CarDescriptionProtocol {
+    var brand: String { get }
+    var modelName: String { get }
+    var year: Int { get }
+    
+    func getDetails() -> String
 }
 
-class Car: Identifiable {
+class Car: Identifiable, CarDescriptionProtocol {
     let id = UUID()
     var brand: String
     var modelName: String
@@ -35,9 +26,13 @@ class Car: Identifiable {
         self.weight = weight
         self.height = height
     }
+    
+    func getDetails() -> String {
+        return "Brand: \(brand)\nModel: \(modelName)\nYear: \(year)"
+    }
 }
 
-class ElectricCar: Car, Electric {
+class ElectricCar: Car {
     var electricEfficiency: Double
     var fullChargeHours: Double
     var autoLevel: Int
@@ -50,9 +45,13 @@ class ElectricCar: Car, Electric {
         super.init(brand: brand, modelName: modelName, year: year, doorCount: doorCount,
                    weight: weight, height: height)
     }
+    
+    override func getDetails() -> String {
+        return super.getDetails() + "\nElectric Efficiency: \(electricEfficiency)\nChargeHour: \(fullChargeHours)"
+    }
 }
 
-class OilCar: Car, Fuel{
+class OilCar: Car {
     var isAutomatic: Bool
     var fuelEfficiency: Double
     var isGasoline: Bool
@@ -67,7 +66,7 @@ class OilCar: Car, Fuel{
     }
 }
 
-class HybridCar: Car, Electric, Fuel {
+class HybridCar: Car {
     var fuelEfficiency: Double
     var isGasoline: Bool
     var autoLevel: Int
@@ -81,18 +80,6 @@ class HybridCar: Car, Electric, Fuel {
     }
 }
 
-extension Fuel {
-    var fuelEfficiencyString: String {
-        String(format: "%.2f"
-               
-               , fuelEfficiency)
-        
-    }
-    var isGasolineString: String {
-        isGasoline ? "가솔린" : "디젤"
-    }
-}
-
 
 let teslaModelX: ElectricCar = ElectricCar(brand: "Tesla", modelName: "Model X", year: 2015, doorCount: 4, weight: 100, height: 40, electricEfficiency: 439, fullChargeHours: 5, autoLevel: 2)
 let teslaModelY: ElectricCar = ElectricCar(brand: "Tesla", modelName: "Model Y", year: 2015, doorCount: 4, weight: 100, height: 40, electricEfficiency: 511 , fullChargeHours: 7, autoLevel: 3)
@@ -102,94 +89,53 @@ let kiaK5: OilCar = OilCar(brand: "KIA", modelName: "K5", year: 2023, doorCount:
 let prius: HybridCar = HybridCar(brand: "Toyota", modelName: "Prius", year: 2023, doorCount: 4, weight: 60, height: 40, fuelEfficiency: 65, isGasoline: true, autoLevel: 0)
 let grandeurHybrid: HybridCar = HybridCar(brand: "Hyundai", modelName: "그렌저 하이브리드", year: 2023, doorCount: 4, weight: 70, height: 35, fuelEfficiency: 16.7, isGasoline: true, autoLevel: 1)
 
-let cars: [Car] = [teslaModelX, teslaModelY, kiaK9, kiaK8, kiaK5, prius, grandeurHybrid ]
+let cars: [Car] = [teslaModelX, teslaModelY, kiaK5, kiaK8, kiaK9, prius, grandeurHybrid]
 
 struct ContentView: View {
-    
     var body: some View {
-        NavigationStack{
+        NavigationStack {
             List {
-                Section(header: Text("Electric Cars")){
-                    ForEach(cars.filter {$0 is ElectricCar}, id: \.id){ car in
-                        NavigationLink(destination: ElectricCarView(electricCar: car as! ElectricCar)) {
-                            Text(car.modelName)
-                        }
-                    }
-                    
-                }
-                Section(header: Text("Oil Cars")){
-                    ForEach(cars.filter {$0 is OilCar}, id: \.id){ car in
-                        NavigationLink(destination: OilCarView(oilCar: car as! OilCar)) {
+                Section(header: Text("Electric Cars")) {
+                    ForEach(cars.filter { $0 is ElectricCar}) { car in
+                        NavigationLink(destination: CarDetailView(car: car)) {
                             Text(car.modelName)
                         }
                     }
                 }
-                Section(header: Text("Hybrid Cars")){
-                    ForEach(cars.filter {$0 is HybridCar}, id: \.id){ car in
-                        NavigationLink(destination: HybridCarView(hybridCar: car as! HybridCar)) {
+                Section(header: Text("Oil Cars")) {
+                    ForEach(cars.filter { $0 is OilCar}) { car in
+                        NavigationLink(destination: CarDetailView(car: car)) {
+                            Text(car.modelName)
+                        }
+                    }
+                }
+                Section(header: Text("Hybrid Cars")) {
+                    ForEach(cars.filter { $0 is HybridCar}) { car in
+                        NavigationLink(destination: CarDetailView(car: car)) {
                             Text(car.modelName)
                         }
                     }
                 }
                 
             }
+            .navigationTitle("Car List")
+        }
+    }
+}
+
+struct CarDetailView: View {
+    let car: Car
+    
+    var body: some View {
+        VStack (alignment: .leading, spacing: 20) {
+            Text(car.modelName)
+                .font(.largeTitle)
+                .fontWeight(.bold)
             
+            Text(car.getDetails())
+                .font(.body)
         }
         .padding()
-    }
-}
-
-struct ElectricCarView: View {
-    var electricCar: ElectricCar
-    
-    var body: some View {
-        VStack {
-            Text("Brand: \(electricCar.brand)")
-            Text("Model: \(electricCar.modelName)")
-            Text("Year: \(electricCar.year)")
-            Text("DoorCount: \(electricCar.doorCount)")
-            Text("Weight: \(electricCar.weight)")
-            Text("Height: \(electricCar.height)")
-            Text("ElectricEfficiency: \(electricCar.electricEfficiency)")
-            Text("FullChargeHours: \(electricCar.fullChargeHours)")
-            Text("AutoLevel: \(electricCar.autoLevel)")
-        }
-    }
-}
-
-struct OilCarView: View {
-    var oilCar: OilCar
-    
-    var body: some View {
-        VStack {
-            Text("Brand: \(oilCar.brand)")
-            Text("Model: \(oilCar.modelName)")
-            Text("Year: \(oilCar.year)")
-            Text("DoorCount: \(oilCar.doorCount)")
-            Text("Weight: \(oilCar.weight)")
-            Text("Height: \(oilCar.height)")
-            Text("IsAutomatic: \(oilCar.isAutomatic)")
-            Text("FuelEfficiency: \(oilCar.fuelEfficiency)")
-            Text("IsGasoline: \(oilCar.isGasoline)")
-        }
-    }
-}
-
-struct HybridCarView: View {
-    var hybridCar: HybridCar
-    
-    var body: some View {
-        VStack {
-            Text("Brand: \(hybridCar.brand)")
-            Text("Model: \(hybridCar.modelName)")
-            Text("Year: \(hybridCar.year)")
-            Text("DoorCount: \(hybridCar.doorCount)")
-            Text("Weight: \(hybridCar.weight)")
-            Text("Height: \(hybridCar.height)")
-            Text("FuelEfficiency: \(hybridCar.fuelEfficiency)")
-            Text("IsGasoline: \(hybridCar.isGasoline)")
-            Text("AutoLevel: \(hybridCar.autoLevel)")
-        }
     }
 }
 
